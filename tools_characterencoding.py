13939b89a2bd6e3487997a534f2a546961b02723
roud <2553112258@qq.com>
2023-07-19 11:26:23 +0800

    update new tool

--- tools_characterencoding.py
+++ tools_characterencoding.py
@@ -1,6 +1,11 @@
 '''
 用于爬虫请求中的响应编码格式转换
 '''
+
+'''
+decode()将其他编码的字符串转成unicode编码格式
+encode()将unicode编码转成其他编码格式
+'''
 def show_utf8(response):
     return response.content.decode('utf-8')
 def show_escape(response):
